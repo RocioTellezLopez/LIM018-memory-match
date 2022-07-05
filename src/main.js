8ac7bf140f287b91122aa893dc1f0db900cb2a8e
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-07-04 19:51:26 -0500

    cambio en el main-funcion replit

--- src/main.js
+++ src/main.js
@@ -1,30 +1,104 @@
 import App from './components/App.js';
 
 
-
-
 document.getElementById('root').appendChild(App());
 
-const cards = document.querySelectorAll('.memoryCard');
-    let firstCard,secondCard; 
-    let lockCard = false;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 50;
+let timerInicial = 50;
+let tiempoRegresivoId = null;
 
-    function flipCard(){
-      //console.log('hola')
-      if(lockCard)return false;
-      this.classList.add('flip');
 
-    if (!firstCard){
-        firstCard=this;
-        return false;
-       }
+let mostrarMovimientos = document.getElementById('moves');
+let mostrarAciertos = document.getElementById('score');
+let mostrarTiempo = document.getElementById('timer')
 
-       secondCard=this;
-       checkForMarch();
+
+const cards = document.querySelectorAll('.memoryCard');   
+let firstCard,secondCard; 
+let lockCard = false;
+
+function contarTiempo(){
+  tiempoRegresivoId = setInterval(()=>{
+    timer--;
+    mostrarTiempo.innerHTML = `Tiempo:${timer} segundos`;
+    if(timer==0){
+      clearInterval(tiempoRegresivoId); 
+      bloquearTarjetas();
     }
+  },1000);
+}
 
-    
-    cards.forEach(card => card.addEventListener('click', flipCard))
+function bloquearTarjetas(){
+  for (let i = 0; i<=19; i++){
+    let tarjetaBloqueada = document.getElementById(i);
+    tarjetaBloqueada.innerHTML = cards[i];
+    tarjetaBloqueada.disabled = true;
+  }
+}
+
+function flipCard(){
+ 
+  if(temporizador == false){
+    contarTiempo();
+    temporizador = true;
+  }
+
+  if(lockCard)return false;
+    this.classList.add('flip');
+   
+  if(!firstCard){
+    firstCard=this;
+    return false;
+  }
+   console.log({lockCard, firstCard});
+  secondCard=this;
+  checkForMatch();
+
+function checkForMatch(){
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+  !isMatch ? disableCards() : resetCards(isMatch);
+ 
+//Incrementar movimientos
+movimientos++;
+mostrarMovimientos.innerHTML= `Movimientos: ${movimientos}`;
+if(firstCard == secondCard){
+  lockCard = 0;
+
+//Aumentar Aciertos
+aciertos++;
+mostrarAciertos.innerHTML = `Aciertos:${aciertos}`;
+  if (aciertos == 10){
+    clearInterval(tiempoRegresivoId);
+    mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`
+    mostrarTiempo.innerHTML = `Tiempo: ${timerInicial - timer} segundos`;
+    mostrarMovimientos.innerHTML =`Movimientos: ${movimientos}`; 
+  }
+ }
+}
+
+function disableCards(){
+  lockCard = true;
+    setTimeout(()=> {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      resetCards();
+    }, 1000);  
+  }
+ 
+function resetCards(isMatch = false){
+  if(isMatch){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard); 
+    }
+     [firstCard,secondCard,lockCard] = [null, null, false];   
+  } 
+} 
+  cards.forEach(card => {
+    card.addEventListener('click', flipCard)
+});
 
 
 
@@ -38,5 +112,11 @@ const cards = document.querySelectorAll('.memoryCard');
 
 
 
+   
+      /*(function shuffle(){
+        cards.forEach (card =>{
+            let randomPos = Math.floor(Math.random() * 10);
+            card.style.order= randomPos;
+        });
+      })();*/
 
-//-------
