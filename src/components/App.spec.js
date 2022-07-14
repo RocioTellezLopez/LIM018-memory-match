54f305e52b259eecc17538da7e64f942a64b33fb
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-07-14 14:23:58 -0500

    Agregué funciones para test

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,4 +1,8 @@
 import App from './App.js';
+import {shuffle} from './App.js';
+import {num} from './App.js';
+
+document.body.innerHTML="<div id='root'></div>"
 
 describe('App', () => {
   it('should render without crashing', () => {
@@ -6,4 +10,16 @@ describe('App', () => {
     expect(el instanceof HTMLElement).toBe(true);
   });
 
+});
+
+describe('shuffle', () => {
+  it('Debe ser una función', () => {
+    expect(typeof shuffle).toBe('function');
+  });
+});
+
+describe('num', () => {
+  it('No debe ser una función', () => {
+    expect(typeof num).not.toBe('function');
+  });
 });
